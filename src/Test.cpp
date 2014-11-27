566dc0202ef5ff74eeb85c44dd2ac7694c0b40d0
Manuel Roth <manuelroth@hotmail.ch>
2014-11-27 16:53:37 +0100

    Added capacity and modifiers with tests

--- src/Test.cpp
+++ src/Test.cpp
@@ -3,6 +3,69 @@
 #include "ide_listener.h"
 #include "xml_listener.h"
 #include "cute_runner.h"
+#include <vector>
+
+//Element Access
+void testAt() {
+	dynArray<int> v { 1, 2 };
+
+	ASSERT_EQUAL(2, v.at(1));
+}
+
+//Iterators
+
+//Capacity
+
+void testEmpty() {
+	dynArray<double> v { };
+
+	ASSERT(v.empty());
+}
+
+void testSize() {
+	dynArray<double> v { 1, 2, 3, 4, 5 };
+
+	ASSERT_EQUAL(5, v.size());
+}
+
+void testCapacity() {
+	dynArray<double> v { 1, 2, 3, 4, 5 };
+
+	ASSERT_EQUAL(5, v.capacity());
+}
+
+//Modifiers
+
+void testClear() {
+	dynArray<double> v { };
+	v.push_back(1.0);
+	v.clear();
+	ASSERT(v.empty());
+}
+
+void testErasePositionIterator() {
+	dynArray<int> v { 1, 2, 3, 4, 5 };
+	v.erase(v.begin());
+	ASSERT_EQUAL(2, *v.begin());
+}
+
+void testErasePositionConstIterator() {
+	dynArray<long int> v { 1, 2, 3, 4, 5 };
+	v.erase(v.cbegin() + 2);
+	ASSERT_EQUAL(4, v.at(2));
+}
+
+void testEraseFirstLastIterator() {
+	dynArray<double> v { 1, 2, 3, 4, 5 };
+	v.erase(v.begin(), v.begin() + 3);
+	ASSERT_EQUAL(4, *v.begin());
+}
+
+void testEraseFirstLastConstIterator() {
+	dynArray<long double> v { 1, 2, 3, 4, 5 };
+	v.erase(v.cbegin(), v.cbegin() + 3);
+	ASSERT_EQUAL(5, v.at(1));
+}
 
 void testPushBack() {
 	dynArray<int> v { };
@@ -12,33 +75,32 @@ void testPushBack() {
 	ASSERT_EQUAL(7, v.at(0));
 }
 
-//Element Access
-void testAt() {
-	dynArray<int> v { 1, 2 };
-	ASSERT_EQUAL(2, v.at(1));
+void testPopBack() {
+	dynArray<long double> v { 1, 2, 3, 4, 5 };
+
+	v.pop_back();
+
+	ASSERT_EQUAL(4, v.size());
 }
 
-//Iterators
+void testResize() {
+	dynArray<int> v { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-
-//Capacity
-
-void testEmpty() {
-	dynArray<double> dynArray { };
-	std::vector<double> stdVector { };
-
-	ASSERT_EQUAL(stdVector.empty(), dynArray.empty());
-}
-
-void testSize() {
-	dynArray<double> dynArray { };
 }
 
 void runAllTests(int argc, char const *argv[]) {
 	cute::suite s { };
-	s.push_back(CUTE(testPushBack));
 	s.push_back(CUTE(testAt));
 	s.push_back(CUTE(testEmpty));
+	s.push_back(CUTE(testSize));
+	s.push_back(CUTE(testCapacity));
+	s.push_back(CUTE(testClear));
+	s.push_back(CUTE(testEraseFirstLastIterator));
+	s.push_back(CUTE(testErasePositionIterator));
+	s.push_back(CUTE(testErasePositionConstIterator));
+	s.push_back(CUTE(testEraseFirstLastConstIterator));
+	s.push_back(CUTE(testPushBack));
+	s.push_back(CUTE(testPopBack));
 	cute::xml_file_opener xmlfile(argc, argv);
 	cute::xml_listener<cute::ide_listener<> > lis(xmlfile.out);
 	cute::makeRunner(lis, argc, argv)(s, "AllTests");
--- src/dynArray.h
+++ src/dynArray.h
@@ -5,22 +5,19 @@
 #include <initializer_list>
 
 template<typename T>
-class dynArray {
+struct dynArray {
 	using array_type = std::vector<T>;
 	using value_type = typename array_type::value_type;
 	using iterator = typename array_type::iterator;
+	using const_iterator = typename array_type::const_iterator;
+	using reverse_iterator = typename array_type::reverse_iterator;
+	using const_reverse_iterator = typename array_type::const_reverse_iterator;
 	using reference = typename array_type::reference;
 	using const_reference = typename array_type::const_reference;
 	using size_type = typename array_type::size_type;
 
-
-	array_type vector { };
-
-public:
-	dynArray(std::initializer_list<T> init) : vector(init) { }
-
-	inline void push_back(value_type const& val) {
-		vector.push_back(val);
+	dynArray(std::initializer_list<T> init) :
+			vector(init) {
 	}
 
 	//Element Access
@@ -33,7 +30,7 @@ public:
 	}
 
 	inline reference operator[](size_type pos) {
-			return vector[pos];
+		return vector[pos];
 	}
 
 	inline const_reference operator[](size_type pos) const {
@@ -65,8 +62,48 @@ public:
 		return vector.begin();
 	}
 
+	inline const_iterator begin() const {
+		return vector.cbegin();
+	}
+
+	inline const_iterator cbegin() {
+		return vector.cbegin();
+	}
+
+	inline reverse_iterator rbegin() {
+		return vector.rbegin();
+	}
+
+	inline const_reverse_iterator rbegin() {
+		return vector.crbegin();
+	}
+
+	inline const_reverse_iterator crbegin() const {
+		return vector.crbegin();
+	}
+
 	inline iterator end() {
 		return vector.end();
+	}
+
+	inline const_iterator end() const {
+		return vector.cend();
+	}
+
+	inline const_iterator cend() const {
+		return vector.cend();
+	}
+
+	inline reverse_iterator rend() {
+		return vector.rend();
+	}
+
+	inline const_reverse_iterator rend() {
+		return vector.crend();
+	}
+
+	inline const_reverse_iterator crend() const {
+		return vector.crend();
 	}
 
 	//Capacity
@@ -79,23 +116,54 @@ public:
 		return vector.size();
 	}
 
-	inline size_type max_size() const {
-		return vector.max_size();
-	}
-
-	inline void reserve( size_type size ){
-		vector.reserve( size );
-	}
-
 	inline size_type capacity() const {
 		return vector.capacity();
 	}
 
-	inline void shrink_to_fit(){
-		vector.shrink_to_fit();
+	//Modifiers
+
+	inline void clear() {
+		vector.clear();
 	}
 
-	//Modifiers
+	inline iterator erase(iterator pos) {
+		return vector.erase(pos);
+	}
+
+	inline iterator erase(const_iterator pos) {
+		return vector.erase(pos);
+	}
+
+	inline iterator erase(iterator first, iterator last) {
+		return vector.erase(first, last);
+	}
+
+	inline iterator erase(const_iterator first, const_iterator last) {
+		return vector.erase(first, last);
+	}
+
+	inline void push_back(const value_type& value) {
+		vector.push_back(value);
+	}
+
+	inline void pop_back() {
+		vector.pop_back();
+	}
+
+	inline void resize(size_type count) {
+		vector.resize(count);
+	}
+
+	inline void resize(size_type count, value_type value = value_type()) {
+		vector.resize(count, value);
+	}
+
+	inline void resize(size_type count, const value_type& value) {
+		vector.resize(count, value);
+	}
+
+private:
+	array_type vector { };
 
 };
 
