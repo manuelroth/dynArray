504512ebece89a863e82620ccc526fb402776a7a
Lukas Martinelli <me@lukasmartinelli.ch>
2014-11-27 09:44:50 +0100

    Added element access methods

--- src/dynArray.h
+++ src/dynArray.h
@@ -5,16 +5,52 @@
 
 template<typename T>
 class dynArray {
-	std::vector<T> vector { };
+	using array_type = std::vector<T>;
+	using value_type = typename array_type::value_type;
+	using iterator = typename array_type::iterator;
+	using reference = typename array_type::reference;
+	using size_type = typename array_type::size_type;
+
+
+	array_type vector { };
 
 public:
-	inline void push_back(T const& val) {
+	inline void push_back(value_type const& val) {
 		vector.push_back(val);
 	}
 
-	inline T& at(int n) {
+	//Element Access
+
+	inline reference at(int n) {
 		return vector.at(n);
 	}
+
+	inline reference back() {
+		return vector.back();
+	}
+
+	inline reference front() {
+		return vector.front();
+	}
+
+	inline reference operator[](int pos) {
+		return vector[pos];
+	}
+
+	//Iterators
+
+	inline iterator begin() {
+		return vector.begin();
+	}
+
+	inline iterator end() {
+		return vector.end();
+	}
+
+	//Capacity
+
+	//Modifiers
+
 };
 
 #endif
